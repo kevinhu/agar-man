28f364bc1fa870e17c4e0a0b59688c8bf683955b
Kevin Hu <kevinhuwest@gmail.com>
2022-03-12 20:58:07 -0500

    Optimized wasm build

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,7 @@
-// use indicatif::ProgressBar;
-// use std::io::{BufWriter, Write};
-use std::str;
-// use std::{
-//     fs::File,
-//     io::{self, BufRead, BufReader},
-//     path::Path,
-// };
+use std::time::Instant;
+use std::{cmp, str};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-
-static MIN_LENGTH: usize = 6;
-
-use std::time::{Duration, Instant};
-
-// fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-//     BufReader::new(File::open(filename)?).lines().collect()
-// }
 
 fn process_line(line: &String) -> String {
     line.to_lowercase()
@@ -42,9 +28,9 @@ fn contained(smaller: &str, larger: &str) -> bool {
     true
 }
 
-fn filter_line(line: &String, seed: &String) -> bool {
+fn filter_line(line: &String, seed: &String, min_length: usize) -> bool {
     line.chars().all(char::is_alphanumeric)
-        & (line.chars().count() >= MIN_LENGTH)
+        & (line.chars().count() >= min_length)
         & contained(line, seed)
 }
 
@@ -59,6 +45,25 @@ fn to_index(c: char) -> usize {
 /** Convert a usize to an ASCII char, such that 0 -> 'a', 1 -> 'b', ..., 25 -> 'z' */
 fn to_char(i: usize) -> char {
     (i + ASCII_OFFSET) as u8 as char
+}
+
+fn array_to_string(array: &Vec<usize>) -> String {
+    let mut s = String::new();
+    for i in 0..array.len() {
+        s.push_str(&to_char(array[i]).to_string());
+    }
+    s
+}
+
+fn compare_char_array(a: &Vec<usize>, b: &Vec<usize>) -> bool {
+    for i in 0..cmp::min(a.len(), b.len()) {
+        if a[i] < b[i] {
+            return true;
+        } else if a[i] > b[i] {
+            return false;
+        }
+    }
+    false
 }
 
 /** Convert string into an array of character counts where a[0] is the count of 'a', a[1] is 'b', etc. */
@@ -96,8 +101,19 @@ impl Trie {
         let seed_counter = to_counter(seed);
 
         let mut results = Vec::new();
+        let mut path = Vec::new();
 
-        self.anagram_recursive(seed_counter, Vec::new(), self, &mut results);
+        let mut current_word = Vec::new();
+        let mut previous_word = Vec::new();
+
+        self.anagram_recursive(
+            seed_counter,
+            &mut path,
+            self,
+            &mut current_word,
+            &mut previous_word,
+            &mut results,
+        );
 
         return results;
     }
@@ -106,22 +122,38 @@ impl Trie {
     fn anagram_recursive(
         &self,
         mut seed_counter: [u32; SIZE],
-        path: Vec<String>,
+        path: &mut Vec<usize>,
         root: &Trie,
+        current_word: &mut Vec<usize>,
+        previous_word: &mut Vec<usize>,
         results: &mut Vec<String>,
-    ) -> Vec<String> {
+    ) -> Vec<Vec<usize>> {
         let mut anagrams = Vec::new();
         if self.end_of_word {
             // if all characters have been used
             if seed_counter == [0; SIZE] {
-                results.push(path.join(" "));
+                results.push(array_to_string(&path));
             }
-            let mut newpath = path.clone();
-            newpath.push("".to_string());
+            path.push(27);
+            let old_word = previous_word.clone();
+            previous_word.clear();
+            previous_word.extend_from_slice(&current_word);
+            current_word.clear();
             // redo search from root
-            let mut node_anagrams =
-                root.anagram_recursive(seed_counter, newpath, root, results);
+            let mut node_anagrams = root.anagram_recursive(
+                seed_counter,
+                path,
+                root,
+                current_word,
+                previous_word,
+                results,
+            );
             anagrams.append(&mut node_anagrams);
+            path.pop();
+            current_word.clear();
+            current_word.extend_from_slice(&previous_word);
+            previous_word.clear();
+            previous_word.extend_from_slice(&old_word);
         }
 
         let mut inner_loop = |i: usize| {
@@ -134,32 +166,25 @@ impl Trie {
             if count == 0 {
                 return;
             }
-            let letter = to_char(i).to_string();
+            if compare_char_array(current_word, previous_word) {
+                return;
+            }
             seed_counter[i] -= 1; // decrement the count of the letter in the seed
 
-            let mut newpath = path.clone();
-            let last_str = newpath.pop();
-
-            let last_last_str = newpath.last();
-
-            if last_str.is_some() {
-                let last_str_unwrap = last_str.unwrap();
-
-                if last_last_str.is_some() {
-                    if last_str_unwrap > *last_last_str.unwrap() {
-                        return;
-                    }
-                }
-
-                newpath.push(last_str_unwrap + &letter);
-            } else {
-                newpath.push(letter);
-            }
+            path.push(i);
+            current_word.push(i);
             // continue search from node
-            let mut node_anagrams =
-                node.unwrap()
-                    .anagram_recursive(seed_counter, newpath, root, results);
+            let mut node_anagrams = node.unwrap().anagram_recursive(
+                seed_counter,
+                path,
+                root,
+                current_word,
+                previous_word,
+                results,
+            );
             anagrams.append(&mut node_anagrams);
+            path.pop(); // pop the letter from the path
+            current_word.pop(); // pop the letter from the current word
             seed_counter[i] = count; // reset counter
         };
 
@@ -174,12 +199,13 @@ impl Trie {
 pub fn generate() -> Vec<String> {
     let dictionary = include_str!("dictionary.txt");
 
+    let min_length = 5;
     let seed = "misunderstanding";
     let seed = str::replace(seed, " ", "").to_string();
 
     let lines: Vec<String> = dictionary.split("\n").map(str::to_string).collect();
 
-    let filter_line_closure = |line: &String| filter_line(line, &seed);
+    let filter_line_closure = |line: &String| filter_line(line, &seed, min_length);
     let processed_lines: Vec<String> = lines // using String as the return type of `to_lowercase`
         .iter()
         .map(process_line)
